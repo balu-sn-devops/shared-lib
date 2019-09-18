b9619bdadf2d718595ea2c14351a9078e490ca72
MohanSairam423 <48183441+MohanSairam423@users.noreply.github.com>
2019-09-18 20:41:40 +0530

    Updated Test Groovy File

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,13 @@
-stage('compile') {
-        snDevOpsStep (stepSysId:'2b36f1d0c70400108c2c02b827c26055')
-        printBuildinfo {
-        	name = "Testing..."
-        }
-        //withEnv( ["PATH+MAVEN=${tool mvn_version}/bin"] ) {
-        	//sh 'mvn clean install -DskipTests'
+#!/usr/bin/env groovy
+def call(Closure body) {
+
+	stage('compile') {
+ 	       snDevOpsStep (stepSysId:'2b36f1d0c70400108c2c02b827c26055')
+	        printBuildinfo {
+	        	name = "Testing..."
+	        }
+	        //withEnv( ["PATH+MAVEN=${tool mvn_version}/bin"] ) {
+	        	//sh 'mvn clean install -DskipTests'
 		//}
     }
+}
