589458176ea737ed3462212a3ce55c649ef73143
MohanSairam423 <48183441+MohanSairam423@users.noreply.github.com>
2019-09-19 11:36:15 +0530

    Updated Jenkins File

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 def call(Closure body) {
 
-	stage('test') {
- 	       snDevOpsStep (stepSysId:'2336f1d0c70400108c2c02b827c26056')
+	stage('testing') {
+ 	       snDevOpsStep (stepSysId:'2336f1d0c70400108c2c02b827c26046')
 	        printBuildinfo {
 	        	name = "Testing..."
 	        }
