066bf8360375562129944d17f3c97726b32e054d
balveer.murki <balveer.murki@servicenow.com>
2021-05-04 09:28:49 +0530

    #BALU-1:UPDATED

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -3,11 +3,16 @@
 import org.apache.commons.lang.StringUtils
 
 final shared-library = library('shared-library')
-def verify(String filter_string, int occurrence) {
+def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
     if (count > occurrence -1) {
         currentBuild.result='UNSTABLE'
     }
+    return this
 }
-return this
+
+def verify(String filter_string, int occurrence) {
+    echo 'verify from shared lib'
+}
+
